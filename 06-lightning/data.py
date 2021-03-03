a9f659b2033af19711543bfbd360a35c7a5d0701
flych3r <xavier_sampaio@alu.ufc.br>
2021-03-03 14:03:36 -0300

    pytorch lightning

---
+++ 06-lightning/data.py
@@ -0,0 +1,65 @@
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
+from torchvision.datasets import MNIST
+
+PATH = '../data/06'
+
+
+def mnist_dataloaders():
+    # ----------------
+    # TRANSFORMS
+    # ----------------
+    # prepare transforms standard to MNIST
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+
+    # ----------------
+    # TRAINING, VAL DATA
+    # ----------------
+    mnist_train = MNIST(PATH, train=True, download=True, transform=transform)
+
+    # train (55,000 images), val split (5,000 images)
+    mnist_train, mnist_val = random_split(mnist_train, [55000, 5000])
+
+    # ----------------
+    # TEST DATA
+    # ----------------
+    mnist_test = MNIST(PATH, train=False, download=True, transform=transform)
+
+    # ----------------
+    # DATALOADERS
+    # ----------------
+    # The dataloaders handle shuffling, batching, etc...
+    mnist_train = DataLoader(mnist_train, batch_size=64)
+    mnist_val = DataLoader(mnist_val, batch_size=64)
+    mnist_test = DataLoader(mnist_test, batch_size=64)
+    return mnist_train, mnist_val, mnist_test
+
+
+class MNISTDataModule(pl.LightningDataModule):
+
+    def setup(self, stage):
+        # transforms for images
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])
+
+        # prepare transforms standard to MNIST
+        mnist_train = MNIST(PATH, train=True, download=True, transform=transform)
+        mnist_test = MNIST(PATH, train=False, download=True, transform=transform)
+
+        self.mnist_train, self.mnist_val = random_split(mnist_train, [55000, 5000])
+        self.mnist_test = mnist_test
+
+    def train_dataloader(self):
+        return DataLoader(self.mnist_train, batch_size=64)
+
+    def val_dataloader(self):
+        return DataLoader(self.mnist_val, batch_size=64)
+
+    def test_dataloader(self):
+        return DataLoader(self.mnist_test, batch_size=64)
---
+++ 06-lightning/model.py
@@ -0,0 +1,75 @@
+import pytorch_lightning as pl
+import torch
+import torch.optim as optimizer
+from torch import nn
+from torch.nn import functional as F
+
+
+class MNISTClassifier(nn.Module):
+
+    def __init__(self):
+        super(MNISTClassifier, self).__init__()
+
+        # mnist images are (1, 28, 28) (channels, width, height)
+        self.layer_1 = torch.nn.Linear(28 * 28, 128)
+        self.layer_2 = torch.nn.Linear(128, 256)
+        self.layer_3 = torch.nn.Linear(256, 10)
+
+    def forward(self, x):
+        batch_size, channels, width, height = x.size()
+
+        # (b, 1, 28, 28) -> (b, 1*28*28)
+        x = x.view(batch_size, -1)
+
+        # layer 1
+        x = self.layer_1(x)
+        x = torch.relu(x)
+
+        # layer 2
+        x = self.layer_2(x)
+        x = torch.relu(x)
+
+        # layer 3
+        x = self.layer_3(x)
+
+        # probability distribution over labels
+        x = torch.log_softmax(x, dim=1)
+
+        return x
+
+
+class LightningMNISTClassifier(pl.LightningModule):
+
+    def __init__(self, backbone=None):
+        super().__init__()
+        if backbone is None:
+            backbone = MNISTClassifier()
+        self.backbone = backbone
+
+    def configure_optimizers(self):
+        optim = optimizer.Adam(self.parameters(), lr=1e-3)
+        return optim
+
+    def cross_entropy_loss(self, logits, labels):
+        return F.nll_loss(logits, labels)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self.backbone(x)
+        loss = self.cross_entropy_loss(logits, y)
+        self.log('train_loss', loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self.backbone(x)
+        loss = self.cross_entropy_loss(logits, y)
+        self.log('val_loss', loss)
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self.backbone(x)
+        loss = self.cross_entropy_loss(logits, y)
+        labels_hat = torch.argmax(logits, dim=1)
+        test_acc = torch.sum(y == labels_hat).item() / (len(y) * 1.0)
+        self.log_dict({'test_loss': loss, 'test_acc': test_acc})
